4ea2e54bd5ebb9194088d97339d9f1990a15dcd4
jah.humes@gmail.com <jah.humes@gmail.com>
2012-09-15 19:25:33 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sebo::Application.config.secret_token = '85bcce36b2929830a8f3bbed4189fab3660637c8135a7c6aab5d56905a3cb9aea0128b84bb1bc79d9eb41db2a5874a867cba2fe0951128cc831fc46f63ab4e03'
